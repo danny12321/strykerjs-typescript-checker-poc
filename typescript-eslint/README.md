9c80567337d2e2991a9ca8d826ebc863753bfbc4
Danny Berkelaar <Danny.Berkelaar@infosupport.com>
2021-10-04 14:40:01 +0200

    Eslint poc

---
+++ typescript-eslint/README.md
@@ -0,0 +1,12 @@
+# Test via eslint 🚀
+
+## Installation 💾
+```sh
+$ npm i
+```
+
+## Run 🏃🏻‍♂️
+Run `npm start`.
+
+## Result 💔
+Only linting errors are detected.
---
+++ typescript-eslint/test.ts
@@ -0,0 +1,8 @@
+function init() {
+    const number: number = 2;
+    const string_good = " " + " ";
+    const stringfault = " " - " ";
+    console.log(number, string_good, stringfault);
+}
+
+init();
